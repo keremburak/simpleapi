00c4bbb3486a4e7161c3288f9e9de78727d6162e
Kerem Burak <keremburak@outlook.com>
2021-11-28 23:23:50 +0300

    initial commit

---
+++ SimpleAPI/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            try
+            {
+                var users = new DataPopulator().GetUsers();
+
+                if (users == null)
+                    return NotFound();
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            try
+            {
+                var user = new DataPopulator().GetUsers().FirstOrDefault(q => q.Id == id);
+
+                if (user == null)
+                    return NotFound();
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ SimpleModel/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+
+    public class DataPopulator
+    {
+        public IEnumerable<User> GetUsers()
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Kerem",
+                    LastName = "Burak",
+                    Address = "Maltepe",
+                    City = "İSTANBUL"
+                },
+                new User
+                {
+                    Id = 2,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Address = "Kadiköy",
+                    City = "İSTANBUL"
+                },
+                new User
+                {
+                    Id = 3,
+                    FirstName = "Mehmet",
+                    LastName = "ŞİMŞEK",
+                    Address = "Foça",
+                    City = "İZMİR"
+                },
+                new User
+                {
+                    Id = 4,
+                    FirstName = "Lale",
+                    LastName = "Oraloğlu",
+                    Address = "Merkez",
+                    City = "Adana"
+                },
+                new User
+                {
+                    Id = 5,
+                    FirstName = "Cüneyt",
+                    LastName = "Arkın",
+                    Address = "Merkez",
+                    City = "ESKİŞEHİR"
+                }
+            };
+
+            return users;
+        }
+    }
+}
